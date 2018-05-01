580bc8d21ff53c7561203f0d07f77917d4d00900
Balte de Wit <balte.dewit@gmail.com>
2018-05-01 08:20:34 +0200

    feat: Transition properties

--- src/atemState.ts
+++ src/atemState.ts
@@ -1,4 +1,7 @@
-import { AtemState as StateObject, Enums as AtemEnum, Commands as AtemCommands, StateObjects } from 'atem-connection'
+import {
+	AtemState as StateObject,
+	Commands as AtemCommands,
+	VideoState } from 'atem-connection'
 import AbstractCommand from 'atem-connection/dist/commands/AbstractCommand' // @todo: should come from main exports
 
 export interface ChangeRecord {
@@ -131,6 +134,85 @@ export class AtemState {
 
 	private resolveTransitionSettingsState (oldState: StateObject, newState: StateObject): Array<AbstractCommand> {
 		const commands: Array<AbstractCommand> = []
+
+		/**
+		 * NOTE(balte - 2018-05-01):
+		 * It's okay to use "as any" here, since all the keys come from the
+		 * transitionObjects anyway, so we may assume everything has the same
+		 * type. Unfortunately TypeScript is not able to understand that if
+		 * we copy properties of the same name between objects of the same type
+		 * the types are still safe.
+		 */
+		for (const mixEffectId in oldState.video.ME) {
+			const oldTransitionSettings = oldState.video.ME[mixEffectId].transitionSettings
+			const newTransitionSettings = oldState.video.ME[mixEffectId].transitionSettings
+
+			const dipProperties: Partial<VideoState.DipTransitionSettings> = {}
+			for (let key in oldTransitionSettings.dip) {
+				if ((oldTransitionSettings.dip as any)[key] !== (newTransitionSettings.dip as any)[key]) {
+					(dipProperties as any)[key] = (newTransitionSettings.dip as any)[key]
+				}
+			}
+			if (Object.keys(dipProperties).length > 0) {
+				let command = new AtemCommands.TransitionDipCommand()
+				command.mixEffect = Number(mixEffectId)
+				command.updateProps(dipProperties)
+				commands.push(command)
+			}
+
+			const dveProperties: Partial<VideoState.DVETransitionSettings> = {}
+			for (let key in oldTransitionSettings.DVE) {
+				if ((oldTransitionSettings.DVE as any)[key] !== (newTransitionSettings.DVE as any)[key]) {
+					(dveProperties as any)[key] = (newTransitionSettings.DVE as any)[key]
+				}
+			}
+			if (Object.keys(dveProperties).length > 0) {
+				let command = new AtemCommands.TransitionDVECommand()
+				command.mixEffect = Number(mixEffectId)
+				command.updateProps(dveProperties)
+				commands.push(command)
+			}
+
+			const mixProperties: Partial<VideoState.MixTransitionSettings> = {}
+			for (let key in oldTransitionSettings.mix) {
+				if ((oldTransitionSettings.mix as any)[key] !== (newTransitionSettings.mix as any)[key]) {
+					(mixProperties as any)[key] = (newTransitionSettings.mix as any)[key]
+				}
+			}
+			if (Object.keys(mixEffectId).length > 0) {
+				let command = new AtemCommands.TransitionMixCommand()
+				command.mixEffect = Number(mixEffectId)
+				command.updateProps(mixProperties)
+				commands.push(command)
+			}
+
+			const stingerProperties: Partial<VideoState.StingerTransitionSettings> = {}
+			for (let key in oldTransitionSettings.stinger) {
+				if ((oldTransitionSettings.stinger as any)[key] !== (newTransitionSettings.stinger as any)[key]) {
+					(stingerProperties as any)[key] = (newTransitionSettings.stinger as any)[key]
+				}
+			}
+			if (Object.keys(stingerProperties).length > 0) {
+				let command = new AtemCommands.TransitionStingerCommand()
+				command.mixEffect = Number(mixEffectId)
+				command.updateProps(stingerProperties)
+				commands.push(command)
+			}
+
+			const wipeProperties: Partial<VideoState.WipeTransitionSettings> = {}
+			for (let key in oldTransitionSettings.wipe) {
+				if ((oldTransitionSettings.wipe as any)[key] !== (newTransitionSettings.wipe as any)[key]) {
+					(wipeProperties as any)[key] = (newTransitionSettings.wipe as any)[key]
+				}
+			}
+			if (Object.keys(wipeProperties).length > 0) {
+				let command = new AtemCommands.TransitionWipeCommand()
+				command.mixEffect = Number(mixEffectId)
+				command.updateProps(wipeProperties)
+				commands.push(command)
+			}
+
+		}
 
 		return commands
 	}
