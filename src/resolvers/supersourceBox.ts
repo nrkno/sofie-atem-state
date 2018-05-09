752c2eb035ad235d3436c2f12f30d8c0ed0353ca
Balte de Wit <balte.dewit@gmail.com>
2018-05-09 07:53:12 +0200

    feat: supersource boxes

---
+++ src/resolvers/supersourceBox.ts
@@ -0,0 +1,30 @@
+import {
+	Commands as AtemCommands, VideoState
+} from 'atem-connection'
+import AbstractCommand from 'atem-connection/dist/commands/AbstractCommand' // @todo: should come from main exports
+import { State as StateObject } from '../'
+
+export function resolveSupersourceBoxState (oldState: StateObject, newState: StateObject): Array<AbstractCommand> {
+	const commands: Array<AbstractCommand> = []
+
+	for (const index in newState.video.superSourceBoxes) {
+		const newBox = newState.video.superSourceBoxes[index]
+		const oldBox = oldState.video.superSourceBoxes[index]
+		const props: Partial<VideoState.SuperSourceBox> = {}
+
+		for (let key in newBox) {
+			if ((newBox as any)[key] !== (oldBox as any)[key]) {
+				(props as any)[key] = (newBox as any)[key]
+			}
+		}
+
+		if (Object.keys(props).length > 0) {
+			const command = new AtemCommands.SuperSourceBoxParametersCommand()
+			command.boxId = Number(index)
+			command.updateProps(props)
+			commands.push(command)
+		}
+	}
+
+	return commands
+}
