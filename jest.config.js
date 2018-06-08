8bf152aa473d9640330ed4ac1246c56363bd938c
Stephan Eriksen <stephanruler@gmail.com>
2018-06-08 22:39:50 +0200

    feat: CircleCI features
    Many changes related to circeci setup. These are bascis for all new
    libraries. Changes to LICENSE, configs, etc.

--- jest.config.js
+++ jest.config.js
@@ -14,13 +14,16 @@ module.exports = {
 	testMatch: [
 		'**/__tests__/**/*.spec.(ts|js)'
 	],
+	testPathIgnorePatterns: [
+		'integrationTests'
+	],	
 	testEnvironment: 'node',
 	coverageThreshold: {
 		global: {
-		  branches: 100,
-		  functions: 100,
-		  lines: 100,
-		  statements: 100
+		  branches: 0,
+		  functions: 0,
+		  lines: 0,
+		  statements: 0
 		}
 	},
 	coverageDirectory: "./coverage/",
