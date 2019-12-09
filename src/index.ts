1cd205fb46f1d818ec93a1a9b0054462c14d38b0
Julian Waller <git@julusian.co.uk>
2019-12-09 11:53:58 +0000

    feat: reexport atem-connection

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,7 @@ export * from './atemState'
 export * from './defaults'
 export * from './enums'
 export * from './state'
+
+// Re-export atem-connection as the version is pinned, and so that users can easily use the same version
+import * as AtemConnection from 'atem-connection'
+export { AtemConnection }
