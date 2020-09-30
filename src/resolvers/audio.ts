0785c88dadd52f69c62e972b14c2e401a1de27c5
Jan Starzak <jan.starzak@gmail.com>
2020-09-30 10:00:29 +0200

    chore: update atem-connection to 2.1.0

--- src/resolvers/audio.ts
+++ src/resolvers/audio.ts
@@ -10,14 +10,16 @@ export function resolveAudioState (oldState: StateObject, newState: StateObject)
 
 	commands.push(...resolveAudioMixerInputsState(oldState, newState))
 
-	if (oldState.audio.master || newState.audio.master) {
-		const oldMaster = oldState.audio.master || Defaults.Audio.Master
-		const newMaster = newState.audio.master || Defaults.Audio.Master
+	if (oldState.audio) {
+		if (oldState.audio.master || newState.audio.master) {
+			const oldMaster = oldState.audio.master || Defaults.Audio.Master
+			const newMaster = newState.audio.master || Defaults.Audio.Master
 
-		const props = diffObject(oldMaster, newMaster)
-		const command = new Commands.AudioMixerMasterCommand()
-		if (command.updateProps(props)) {
-			commands.push(command)
+			const props = diffObject(oldMaster, newMaster)
+			const command = new Commands.AudioMixerMasterCommand()
+			if (command.updateProps(props)) {
+				commands.push(command)
+			}
 		}
 	}
 
@@ -27,14 +29,16 @@ export function resolveAudioState (oldState: StateObject, newState: StateObject)
 export function resolveAudioMixerInputsState (oldState: StateObject, newState: StateObject): Array<Commands.ISerializableCommand> {
 	const commands: Array<AtemCommands.ISerializableCommand> = []
 
-	for (const index of getAllKeysNumber(oldState.audio.channels, newState.audio.channels)) {
-		const oldChannel = oldState.audio.channels[index] || Defaults.Audio.Channel
-		const newChannel = newState.audio.channels[index] || Defaults.Audio.Channel
+	if (oldState.audio && newState.audio) {
+		for (const index of getAllKeysNumber(oldState.audio.channels, newState.audio.channels)) {
+			const oldChannel = oldState.audio.channels[index] || Defaults.Audio.Channel
+			const newChannel = newState.audio.channels[index] || Defaults.Audio.Channel
 
-		const props = diffObject(oldChannel, newChannel)
-		const command = new Commands.AudioMixerInputCommand(index)
-		if (command.updateProps(props)) {
-			commands.push(command)
+			const props = diffObject(oldChannel, newChannel)
+			const command = new Commands.AudioMixerInputCommand(index)
+			if (command.updateProps(props)) {
+				commands.push(command)
+			}
 		}
 	}
 
