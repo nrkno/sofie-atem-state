f235a70c009844252291e8b288e63205c9c97c72
Julian Waller <git@julusian.co.uk>
2021-04-18 18:41:33 +0100

    feat: remaining classic audio

--- src/resolvers/classic-audio.ts
+++ src/resolvers/classic-audio.ts
@@ -1,4 +1,4 @@
-import { Commands as AtemCommands, Commands } from 'atem-connection'
+import { AudioState, Commands as AtemCommands, Commands } from 'atem-connection'
 import { State as StateObject } from '../state'
 import { getAllKeysNumber, diffObject, fillDefaults } from '../util'
 import * as Defaults from '../defaults'
@@ -9,16 +9,49 @@ export function resolveClassicAudioState(
 	newState: PartialDeep<StateObject>
 ): Array<Commands.ISerializableCommand> {
 	const commands: Array<AtemCommands.ISerializableCommand> = []
-	if (!newState.audio) return commands
-
-	commands.push(...resolveClassicAudioMixerInputsState(oldState, newState))
 
 	if (oldState.audio || newState.audio) {
+		commands.push(...resolveClassicAudioMixerOutputsState(oldState, newState))
+		commands.push(...resolveClassicAudioMixerInputsState(oldState, newState))
+	}
+
+	return commands
+}
+
+export function resolveClassicAudioMixerOutputsState(
+	oldState: PartialDeep<StateObject>,
+	newState: PartialDeep<StateObject>
+): Array<Commands.ISerializableCommand> {
+	const commands: Array<AtemCommands.ISerializableCommand> = []
+
+	{
 		const oldMaster = fillDefaults(Defaults.Audio.Master, oldState.audio?.master)
 		const newMaster = fillDefaults(Defaults.Audio.Master, newState.audio?.master)
 
-		const props = diffObject(oldMaster, newMaster)
+		const props = diffObject<AudioState.ClassicAudioMasterChannel>(oldMaster, newMaster)
 		const command = new Commands.AudioMixerMasterCommand()
+		if (command.updateProps(props)) {
+			commands.push(command)
+		}
+	}
+
+	{
+		const oldMonitor = fillDefaults(Defaults.Audio.Monitor, oldState.audio?.monitor)
+		const newMonitor = fillDefaults(Defaults.Audio.Monitor, newState.audio?.monitor)
+
+		const props = diffObject<AudioState.ClassicAudioMonitorChannel>(oldMonitor, newMonitor)
+		const command = new Commands.AudioMixerMonitorCommand()
+		if (command.updateProps(props)) {
+			commands.push(command)
+		}
+	}
+
+	{
+		const oldHeadphones = fillDefaults(Defaults.Audio.Headphones, oldState.audio?.headphones)
+		const newHeadphones = fillDefaults(Defaults.Audio.Headphones, newState.audio?.headphones)
+
+		const props = diffObject<AudioState.ClassicAudioHeadphoneOutputChannel>(oldHeadphones, newHeadphones)
+		const command = new Commands.AudioMixerHeadphonesCommand()
 		if (command.updateProps(props)) {
 			commands.push(command)
 		}
@@ -33,16 +66,14 @@ export function resolveClassicAudioMixerInputsState(
 ): Array<Commands.ISerializableCommand> {
 	const commands: Array<AtemCommands.ISerializableCommand> = []
 
-	if (oldState.audio || newState.audio) {
-		for (const index of getAllKeysNumber(oldState.audio?.channels, newState.audio?.channels)) {
-			const oldChannel = fillDefaults(Defaults.Audio.Channel, oldState.audio?.channels?.[index])
-			const newChannel = fillDefaults(Defaults.Audio.Channel, newState.audio?.channels?.[index])
+	for (const index of getAllKeysNumber(oldState.audio?.channels, newState.audio?.channels)) {
+		const oldChannel = fillDefaults(Defaults.Audio.Channel, oldState.audio?.channels?.[index])
+		const newChannel = fillDefaults(Defaults.Audio.Channel, newState.audio?.channels?.[index])
 
-			const props = diffObject(oldChannel, newChannel)
-			const command = new Commands.AudioMixerInputCommand(index)
-			if (command.updateProps(props)) {
-				commands.push(command)
-			}
+		const props = diffObject<AudioState.ClassicAudioChannel>(oldChannel, newChannel)
+		const command = new Commands.AudioMixerInputCommand(index)
+		if (command.updateProps(props)) {
+			commands.push(command)
 		}
 	}
 
