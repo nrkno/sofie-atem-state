ae3355808c06a717bdc2c8b1643f9f8cc9fae04d
Julian Waller <julian@superfly.tv>
2021-05-25 22:34:09 +0100

    feat: move atem-connection to be a peer-dependency (#19)

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,3 @@ export * from './atemState'
 export * as Defaults from './defaults'
 export * as Enums from './enums'
 export * from './state'
-
-// Re-export atem-connection as the version is pinned, and so that users can easily use the same version
-import * as AtemConnection from 'atem-connection'
-export { AtemConnection }
-export { Commands, Enums as AtemEnums } from 'atem-connection'
