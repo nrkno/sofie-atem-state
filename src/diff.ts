e44695301f985401d96f6d1be426436b162589f3
Julian Waller <git@julusian.co.uk>
2022-03-21 23:49:56 +0000

    wip: diff all example

--- src/diff.ts
+++ src/diff.ts
@@ -1,3 +1,14 @@
+export function DiffAllObject(): Required<SectionsToDiff> {
+	// TODO - use a DeepRequired to ensure we don't miss anything
+	return {
+		auxiliaries: 'all',
+		multiviewer: {
+			properties: true,
+			windows: 'all',
+		},
+	}
+}
+
 export interface SectionsToDiff {
 	auxiliaries?: DiffAuxiliaries
 	multiviewer?: DiffMultiViewer | DiffMultiViewer[]
